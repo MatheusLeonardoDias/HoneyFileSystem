7047010cae26cff5c5a0d37f1d6b51f82027bc02
EmerHoff <emerson_h2010@hotmail.com>
2017-11-16 20:42:03 -0200

    classes fat e dir
    classes fat e dir

--- directory_entry.h
+++ directory_entry.h
@@ -1,4 +1,29 @@
 class __attribute__((__packed__)) directory_entry{
 	public:
-		
+		unsigned short name_file;
+		unsigned short attribute_file;
+		unsigned short creation_time;
+		unsigned short creation_date;
+		unsigned short last_acess_date;
+		unsigned short last_modification_time;
+		unsigned short last_modification_date;
+		unsigned short first_cluster;
+		unsigned short size_file;
 };
+
+directory_entry::directory_entry(unsigned short name_file, unsigned short attribute_file,
+                                 unsigned short creation_time, unsigned short creation_date,
+                                 unsigned short last_acess_date, unsigned short last_modification_time,
+                                 unsigned short last_modification_date, unsigned short first_cluster,
+                                 unsigned short size_file){
+
+	this->name_file = name_file;
+	this->attribute_file = attribute_file;
+	this->creation_time = creation_time;
+	this->creation_date = creation_date;
+	this->last_acess_date = last_acess_date;
+	this->last_modification_time = last_modification_time;
+	this->last_modification_date = last_modification_date;
+	this->first_cluster = first_cluster;
+	this->size_file = size_file;
+}
--- fat_table.h
+++ fat_table.h
@@ -1,4 +1,19 @@
 class __attribute__((__packed__)) fat_table{
 	public:
-		
+	    unsigned char tabela_FAT[bytes_per_sector];
+        unsigned int fat_deslocamento;
+        unsigned int fat_setor;
+        unsigned int desloc_entrada;
+        unsigned short valor_tabela;
 };
+
+fat_table::fat_table(unsigned char tabela_FAT[bytes_per_sector], unsigned int fat_deslocamento,
+                     unsigned int fat_setor, unsigned int desloc_entrada,
+                     unsigned short valor_tabela){
+
+	this->tabela_FAT[bytes_per_sector] = tabela_FAT[bytes_per_sector];
+	this->fat_deslocamento = cluster_atual * 2;
+	this->fat_setor = fat_primeiro_setor + (fat_deslocamento / bytes_per_sector);
+	this->desloc_entrada = fat_deslocamento % bytes_per_sector;;
+	this->valor_tabela = *(unsigned short*)&tabela_FAT[desloc_entrada];;
+}
